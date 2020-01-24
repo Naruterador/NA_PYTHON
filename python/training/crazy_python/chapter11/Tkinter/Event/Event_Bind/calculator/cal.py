a8cd3087e43c228101793dd66fa002da9745ee17
Naruterador <103258937@qq.com>
2020-01-24 17:03:33 +0800

    update

--- python/training/crazy_python/chapter11/Tkinter/Event/Event_Bind/calculator/cal.py
+++ python/training/crazy_python/chapter11/Tkinter/Event/Event_Bind/calculator/cal.py
@@ -2,6 +2,7 @@
 
 
 from tkinter import *
+import math
 import sys
 
 class calculator:
@@ -10,10 +11,17 @@ class calculator:
 		self.master = master
 		self.initWidgets()
 		self.p = StringVar()
+		self.a = 0
+		self.b = 0
+		self.c = 0
+		self.result = 0
+		self.flag = True
+		self.operator = ''
+
 
 	def initWidgets(self):
 		#创建一个输入框
-		self.e = Entry(relief=GROOVE,font=('Courier New',24),width=21,justify=RIGHT)
+		self.e = Entry(relief=GROOVE,font=('Courier New',24),width=22,justify=RIGHT)
 		#对输入组件开启布局pack，并置于最顶端
 		self.e.pack(side=TOP,fill=BOTH)
 
@@ -30,7 +38,7 @@ class calculator:
 		for i in range(len(bottonnames)):
 			#创建Botton，如果Botton=0，就占2格
 			if bottonnames[i] == '0':
-				self.buttonlist = Button(p,text=bottonnames[i],font=('Vendana',20),width = 12)
+				self.buttonlist = Button(p,text=bottonnames[i],font=('Vendana',20),width = 14)
 				self.buttonlist.grid(row = 4,column = 0,columnspan = 2)
 				self.buttonlist.bind('<Button-1>',self.keyfunction)
 			elif bottonnames[i] == '.':
@@ -47,16 +55,101 @@ class calculator:
 				self.buttonlist.bind('<Button-1>',self.keyfunction)
 	
 	def keyfunction(self,event):
-		result = 0
-		a = 0
-		b = 0
-		#获取用户当前的按的按钮是什么
-		if event.widget['text'] == '1':
-			self.p.set = event.widget['text']
-			#在文本框中插入用户所输入的值
-			self.e.insert(END,'1')
+		numbers = ['7','8','9','4','5','6','1','2','3','0','.']
+		operators = ['+','-','*','÷','%']
+
+		if self.flag:
+			if event.widget['text'] in numbers:
+				#在文本框中插入用户所输入的值
+				self.e.insert(END,event.widget['text'])
+		else:
+			if event.widget['text'] in numbers:
+				#在文本框中插入用户所输入的值
+				self.e.delete(0,END)
+				self.e.insert(END,event.widget['text'])
+				self.flag = True
 
 
+		if event.widget['text'] == '+/-':
+			if self.e.get():
+				try:
+					change = int(self.e.get())
+					change = change * -1
+					self.e.delete(0,END)
+					self.e.insert(END,change)
+				except:
+					change = float(self.e.get())
+					change = change * -1
+					self.e.delete(0,END)
+					self.e.insert(END,change)
+
+
+		if event.widget['text'] in operators:
+			
+			if event.widget['text'] == '+':
+				self.operator = '+'
+			elif event.widget['text'] == '-':
+				self.operator = '-'
+			elif event.widget['text'] == '*':
+				self.operator = '*'
+			elif event.widget['text'] == '÷':
+				self.operator = '/'
+			elif event.widget['text'] == '%':
+				self.operator = '%'
+
+			
+			self.a = self.e.get()
+			self.flag = False
+		
+
+
+		if event.widget['text'] == '=':
+			self.b = self.e.get()
+
+			if self.flag:
+				self.c = self.b
+				self.flag = False 
+			self.e.delete(0,END)
+
+			if self.operator == '':
+				self.e.delete(0,END)
+			elif self.operator == '+':
+				try:
+					self.result = int(self.a) + int(self.b)
+				except:
+					self.result = float(self.a) + float(self.b)
+			elif self.operator == '-':
+				try:
+					self.result = int(self.a) - int(self.b)
+				except:
+					self.result = float(self.a) - float(self.b)
+			elif self.operator == '*':
+				try:
+					self.result = int(self.a) * int(self.b)
+				except:
+					self.result = float(self.a) * float(self.b)
+			elif self.operator == '÷':
+				try:
+					self.result = int(self.a) / int(self.b)
+				except:
+					self.result = float(self.a) / float(self.b)
+			elif self.operator == '%':
+				try:
+					self.result = int(self.a) % int(self.b)
+				except:
+					self.result = float(self.a) / float(self.b)
+
+
+			self.e.insert(END,self.result)
+			self.a = self.c
+			self.flag = True
+
+		if event.widget['text'] == 'AC':
+			self.a = ''
+			self.b = ''
+			self.result = 0
+			self.e.delete(0,END)
+			self.flag = True
 
 
 windows = Tk()
